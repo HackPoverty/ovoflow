2ea5c50e9d62d78c83de3e7b1b9a50ee4af18984
Viet Hung Nguyen <vhn@sfu.ca>
2023-06-27 07:06:37 -0700

    Paginated farmers list

--- app/(technician)/farmers/page.tsx
+++ app/(technician)/farmers/page.tsx
@@ -1,10 +1,63 @@
+import { FarmerListItem } from "@/components/FarmerListItem";
 import MenuButton from "@/components/navigation/MenuButton";
 import NavigationBar from "@/components/navigation/NavigationBar";
+import { jsonApiFetchPaginated } from "@/lib/axios";
+import { Node } from "@/types/highLevel";
+import { FARMER_ROLE_ID, Farmer } from "@/types/user";
+import Link from "next/link";
 
-export default function FarmerList() {
+const PAGE_LIMIT = 6
+
+type Props = {
+  searchParams: {
+    offset?: number
+  }
+}
+
+type Result = Pick<Node<Farmer>, "id" | "fieldFarmerLastVisited" | "name">
+
+export default async function FarmerList({ searchParams }: Props) {
+  const offset = searchParams.offset || 0;
+  const prev = Math.max(0, +offset - PAGE_LIMIT);
+  const next = +offset + PAGE_LIMIT;
+
+
+  const { data, isFirst, isLast } = await jsonApiFetchPaginated<Result>("user/user", {
+    "filter[roles.meta.drupal_internal__target_id]": FARMER_ROLE_ID,
+    "fields[user--user]": "field_farmer_last_visited,name",
+    "sort": "-field_farmer_last_visited,name",
+  }, PAGE_LIMIT, searchParams.offset);
+
+  const now = new Date();
+
   return <>
     <NavigationBar title="Farmers List" button={<MenuButton />} />
-    <main className="">
+    <main className="py-6">
+      <div>
+        {
+          data.map(farmer => <FarmerListItem
+            key={farmer.id}
+            name={farmer.name}
+            now={now}
+            lastVisitDate={farmer.fieldFarmerLastVisited
+              ? new Date(farmer.fieldFarmerLastVisited) : undefined}
+          />)
+        }
+      </div>
+      <div className="flex justify-center gap-6 mt-6">
+        {/* Previous */}
+        <Link href={`?offset=${prev}`} className={`btn btn-circle ${isFirst ? "btn-disabled" : "btn-accent"}`}>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+          </svg>
+        </Link>
+        {/* Next */}
+        <Link href={`?offset=${next}`} className={`btn btn-circle ${isLast ? "btn-disabled" : "btn-accent"}`}>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+          </svg>
+        </Link>
+      </div>
     </main>
   </>
 }
--- lib/axios.ts
+++ lib/axios.ts
@@ -27,3 +27,31 @@ export async function jsonApiFetch<Type>(resource: string, params?: Record<strin
     } else throw error;
   }
 }
+
+export async function jsonApiFetchPaginated<Type>(resource: string, params?: Record<string, any>, limit = 10, offset = 0) {
+  const token = cookies().get("token")?.value;
+  const headers = token ? { Authorization: `Bearer ${token}` } : {}
+
+  try {
+    const response = await jsonApi.get(resource, { params: {
+      ...params,
+      "page[limit]": limit,
+      "page[offset]": offset,
+    }, headers })
+    const data = response.data
+    const isFirst = offset === 0 || data.links?.prev === undefined;
+    const isLast = data.links?.next === undefined;
+    return {
+      data: jsonDeserialize<Type[]>(data),
+      isFirst,
+      isLast,
+    }
+  } catch (error) {
+    let status = 500;
+    if (axios.isAxiosError(error))
+      status = error.response?.status || 500;
+    if (status >= 400 || status < 500) {
+      redirect("/logout")
+    } else throw error;
+  }
+}
