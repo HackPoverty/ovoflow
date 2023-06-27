29b819d9b0fe116300aa39669cff427d85980405
Viet Hung Nguyen <vhn@sfu.ca>
2023-06-27 05:18:56 -0700

    Add more navigation links

--- app/(technician)/farmers/page.tsx
+++ app/(technician)/farmers/page.tsx
@@ -3,8 +3,8 @@ import NavigationBar from "@/components/navigation/NavigationBar";
 
 export default function FarmerList() {
   return <>
-    <NavigationBar title="Farmer Lists" button={<MenuButton />} />
-    <main className="px-6 pt-6 pb-16">
+    <NavigationBar title="Farmers List" button={<MenuButton />} />
+    <main className="">
     </main>
   </>
 }
