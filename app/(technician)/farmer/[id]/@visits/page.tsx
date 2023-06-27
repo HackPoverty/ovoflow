1090b81ac7f86a38992d58bda876b7d94c565ba9
Viet Hung Nguyen <vhn@sfu.ca>
2023-06-27 11:23:24 -0700

    Technician visit detail page

--- app/(technician)/farmer/[id]/@visits/page.tsx
+++ app/(technician)/farmer/[id]/@visits/page.tsx
@@ -3,6 +3,7 @@ import { jsonApiFetchPaginated } from "@/lib/axios";
 import { enDate } from "@/lib/formatter";
 import { TechnicianVisit } from "@/types/content";
 import { Node } from "@/types/highLevel";
+import Link from "next/link";
 
 const PAGE_LIMIT = 6;
 
@@ -18,10 +19,6 @@ type Props = {
 type Result = Pick<Node<TechnicianVisit>, "created" | "id">
 
 export default async function Visits({ params, searchParams }: Props) {
-  console.log({
-    id: params.id,
-    offset: searchParams.visit_offset || 0
-  })
   const offset = +(searchParams.visit_offset || 0);
   const { data, isFirst, isLast } = await jsonApiFetchPaginated<Result>("node/technician_visit", {
     "filter[field_for_farmer.id]": params.id,
@@ -31,9 +28,11 @@ export default async function Visits({ params, searchParams }: Props) {
 
   return <>
     <div className="my-4">
-      {data.map(entry => <div key={entry.id} className="px-6 py-2 bg-base-200 my-1">
-        <p>{enDate.format(new Date(entry.created))}</p>
-      </div>)}
+      {data.map(entry => <Link key={entry.id} href={`/visit/${entry.id}`}>
+        <div className="px-6 py-2 bg-base-200 my-1">
+          <p>{enDate.format(new Date(entry.created))}</p>
+        </div>
+      </Link>)}
     </div>
     <div className="flex gap-4 justify-center">
       <PaginationButtons
