87f83a99d6cf8ce62ce1be02451e5d5fec4600a6
Viet Hung Nguyen <vhn@sfu.ca>
2023-06-29 05:16:00 -0700

    Post daily journal

--- app/(technician)/farmer/[id]/(dashboard)/@visits/loading.tsx
+++ app/(technician)/farmer/[id]/(dashboard)/@visits/loading.tsx
@@ -1,7 +1,7 @@
-import TechnicianVisitListItemSkeleton from "@/components/technician/TechnicianVisitListItemSkeleton";
+import LoadingSpinner from "@/components/loading/LoadingSpinner";
 
 export default function Loading() {
-  return <div className="bg-base-200 flex flex-col items-center">
-    
+  return <div className="bg-base-200 flex flex-col items-center gap-4">
+    <LoadingSpinner />
   </div>
 }
