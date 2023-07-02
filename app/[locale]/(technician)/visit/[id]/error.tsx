8b1272e69baa03dd4edf6af374d4cd4e5b8c9e9c
Viet Hung Nguyen <vhn@sfu.ca>
2023-07-01 17:26:39 -0700

    Translation for farm visit

--- app/[locale]/(technician)/visit/[id]/error.tsx
+++ app/[locale]/(technician)/visit/[id]/error.tsx
@@ -1,9 +1,12 @@
 "use client"
 
 import ErrorSection from "@/components/error/ErrorSection"
+import { useTranslations } from "next-intl"
 
 export default function Error({ reset }: { error: Error, reset: () => void }) {
+  const t = useTranslations("TechnicianVisit.Error")
   return <main className="flex flex-col items-center justify-center gap-2 flex-1 text-error">
-    <ErrorSection label="" />
+    <ErrorSection label={t("message")} />
+    <button className="btn">{t("try again")}</button>
   </main>
 }
--- app/[locale]/(technician)/visit/[id]/loading.tsx
+++ app/[locale]/(technician)/visit/[id]/loading.tsx
@@ -1,7 +1,9 @@
 import LoadingSpinner from "@/components/loading/LoadingSpinner";
+import { useTranslations } from "next-intl";
 
 export default function Loading() {
+  const t = useTranslations("TechnicianVisit")
   return <main className="flex flex-col items-center justify-center gap-6 flex-1">
-    <LoadingSpinner label="" />
+    <LoadingSpinner label={t("loading")} />
   </main>
 }
