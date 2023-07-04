267b86c05c63736282cad661a67c3ced4e8c2be2
Viet Hung Nguyen <vhn@sfu.ca>
2023-07-03 19:36:45 -0700

    Remove unused formatter

--- components/technician/TechnicianVisitListItem.tsx
+++ components/technician/TechnicianVisitListItem.tsx
@@ -1,6 +1,6 @@
-import { enFullDate } from "@/lib/formatter"
 import { TechnicianVisit } from "@/types/content"
 import { Node } from "@/types/highLevel"
+import { useFormatter } from "next-intl"
 import Link from "next-intl/link"
 
 type Props = {
@@ -8,10 +8,15 @@ type Props = {
 }
 
 export default function TechnicianVisitListItem({ visit }: Props) {
+  const formatter = useFormatter();
   return <Link key={visit.id} href={`/visit/${visit.id}`}>
     <div className="px-6 py-2 bg-base-200 my-1">
       <p className="font-semibold">{visit.title}</p>
-      <p className="text-sm">{enFullDate.format(new Date(visit.created))}</p>
+      <p className="text-sm">{formatter.dateTime(new Date(visit.created), {
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+      })}</p>
     </div>
   </Link>
 }
