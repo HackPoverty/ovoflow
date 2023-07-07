97f82507fcb5f3a0ebab568f038dfe887c06997b
Viet Hung Nguyen <vhn@sfu.ca>
2023-07-07 03:34:43 -0700

    Setup PWA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.css';
 import { NextIntlClientProvider } from 'next-intl';
 import type { AppProps } from 'next/app';
 import { Poppins } from 'next/font/google';
+import Head from 'next/head';
 
 const poppins = Poppins({
   subsets: ["latin-ext"],
@@ -15,6 +16,12 @@ export default function App({ Component, pageProps }: AppProps) {
           font-family: ${poppins.style.fontFamily};
         }
       `}</style>
+    <Head>
+      <meta
+        name='viewport'
+        content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover'
+      />
+    </Head>
     <NextIntlClientProvider messages={pageProps.messages}>
       <Component {...pageProps} />
     </NextIntlClientProvider>
