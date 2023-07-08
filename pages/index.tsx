eb591b1acef16a56121ecd189c4f0d20ec149722
Viet Hung Nguyen <vhn@sfu.ca>
2023-07-08 12:02:15 -0700

    Fix index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,21 @@
-import PublicRoute from "@/components/layouts/PublicRoute";
+import { decodeToken, TECHNICIAN_ROLE } from "@/lib/user";
+import { getCookie } from "cookies-next";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function Index() {
-  return <PublicRoute />
+  const router = useRouter();
+  
+  useEffect(() => {
+    const token = getCookie("token")?.toString();
+    if (!token) {
+      router.replace("/logout");
+      return;
+    }
+    const user = decodeToken(token).user;
+    if (user.role === TECHNICIAN_ROLE) router.replace("/technician");
+    else router.replace("/technician");
+  }, [router])
+
+  return null;
 }
