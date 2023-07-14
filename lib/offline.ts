503675cae204f4e772970a18719e4423b557fa5a
Viet Hung Nguyen <vhn@sfu.ca>
2023-07-14 10:14:22 -0700

    Offline submission with IndexedDB

---
+++ lib/offline.ts
@@ -0,0 +1,29 @@
+import { FarmerJournalSchema } from "@/components/forms/farmer-journal/schema";
+import { TechnicianVisitFormSchema } from "@/components/forms/technician-visit/schema";
+import Dexie, { Table } from "dexie";
+
+export interface TechnicianVisitFormEntry {
+  id?: number;
+  value: TechnicianVisitFormSchema
+}
+
+export interface FarmerJournalEntry {
+  id?: number;
+  value: FarmerJournalSchema
+}
+
+
+export class OvoflowDexie extends Dexie {
+  technicianVisit!: Table<TechnicianVisitFormEntry>
+  farmerJournal!: Table<FarmerJournalEntry>
+
+  constructor() {
+    super('ovoflow-databhase');
+    this.version(1).stores({
+      technicianVisit: '++id, value',
+      farmerJournal: '++id, value'
+    });
+  }
+}
+
+export const offlineDB = new OvoflowDexie();
--- pages/logout.tsx
+++ pages/logout.tsx
@@ -1,4 +1,3 @@
-import { OVOFLOW_JOURNAL_KEY, OVOFLOW_TECHNICIAN_VISIT_KEY, useLocalStorage } from "@/hooks/useLocalStorage";
 import { getLocaleStaticsProps } from "@/lib/i18n";
 import { deleteCookie } from "cookies-next";
 import { useTranslations } from "next-intl";
@@ -10,18 +9,14 @@ export default function Logout() {
   const t = useTranslations("Navigation")
   const router = useRouter();
   const [_, startTransition] = useTransition()
-  const { removeValue: removeJournal } = useLocalStorage(OVOFLOW_JOURNAL_KEY)
-  const { removeValue: removeChecklist } = useLocalStorage(OVOFLOW_TECHNICIAN_VISIT_KEY)
 
   useEffect(() => {
     startTransition(() => {
       deleteCookie("token")
       deleteCookie("uid")
-      removeJournal()
-      removeChecklist()
       router.replace("/login")
     })
-  }, [removeChecklist, removeJournal, router])
+  }, [router])
 
   return <>
     <Head>
