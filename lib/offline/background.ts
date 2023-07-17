ab57791065020d762b2daf8525d895dbcb1c1688
Viet Hung Nguyen <vhn@sfu.ca>
2023-07-16 22:35:18 -0700

    Asynchronus bulk upload

--- lib/offline/background.ts
+++ lib/offline/background.ts
@@ -4,18 +4,16 @@ import { offlineDB } from "./db";
 
 export async function submitOfflineJournals() {
   const data = await offlineDB.farmerJournal.toArray();
-  for (const entry of data) {
-    console.log("post journal", entry.id);
+  await Promise.all(data.map(async (entry) => {
     await jsonApiPost("node/farmer_daily_journal", processJournal(entry.value))
     await offlineDB.farmerJournal.delete(entry.id!);
-  }
+  }));
 }
 
 export async function submitOfflineTechnicals() {
   const data = await offlineDB.technicianVisit.toArray();
-  for (const entry of data) {
-    console.log("post technician visit", entry.id);
+  await Promise.all(data.map(async (entry) => {
     await jsonApiPost("node/technician_visit", processTechnical(entry.value, entry.farmerId))
     await offlineDB.technicianVisit.delete(entry.id!);
-  }
+  }))
 }
