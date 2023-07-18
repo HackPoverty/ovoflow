2f4c22f8946787759b21af08bbce37128078591b
Viet Hung Nguyen <vhn@sfu.ca>
2023-07-18 12:28:17 -0700

    Configure prettier

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-})
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   i18n: {
-    locales: ['en', 'pt'],
-    defaultLocale: 'en',
-  }
-}
+    locales: ["en", "pt"],
+    defaultLocale: "en",
+  },
+};
 
-module.exports = withPWA(nextConfig)
+module.exports = withPWA(nextConfig);
